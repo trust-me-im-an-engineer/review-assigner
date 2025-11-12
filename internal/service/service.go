9e262a8e3202a4d3f417d072dc43b90b7a4e6fa2
trust-me-im-an-engineer <isavelij4@gmail.com>
2025-11-13 01:19:22 +0300

    clarify service having no interface

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,6 +2,8 @@ package service
 
 import "review-assigner/internal/storage"
 
+// Service is considered to be a core layer of witch only one could exist,
+// therefore it doesn't use interface
 type Service struct {
 	storage storage.Storage
 }
