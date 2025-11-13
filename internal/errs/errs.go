3c3c77f09f05e9aba8f4a1878f74b78f27f29dc0
trust-me-im-an-engineer <isavelij4@gmail.com>
2025-11-13 13:20:58 +0300

    errs

---
+++ internal/errs/errs.go
@@ -0,0 +1,29 @@
+package errs
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	PullRequestMergedErr = errors.New("cannot reassign on merged PR")
+	NotAssignedErr       = errors.New("reviewer is not assigned to this PR")
+	NoCandidateErr       = errors.New("no active replacement candidate in team")
+	NotFoundErr          = errors.New("resource not found")
+)
+
+type TeamExistsError struct {
+	TeamID string
+}
+
+func (e TeamExistsError) Error() string {
+	return fmt.Sprintf("%s already exists", e.TeamID)
+}
+
+type PullRequestExistsError struct {
+	PullRequestID string
+}
+
+func (e PullRequestExistsError) Error() string {
+	return fmt.Sprintf("%s already exists", e.PullRequestID)
+}
