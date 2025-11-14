101fb055a03eb0438243a006ae14e31142ae5fff
trust-me-im-an-engineer <isavelij4@gmail.com>
2025-11-15 02:01:33 +0300

    reduce nesting

--- internal/storage/postgres/team.go
+++ internal/storage/postgres/team.go
@@ -2,13 +2,31 @@ package postgres
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+
+	"review-assigner/internal/errs"
 	"review-assigner/internal/model"
 )
 
+// AddTeam inserts a new team into the database.
 func (s *Storage) AddTeam(ctx context.Context, name string) (string, error) {
-	//TODO implement me
-	panic("implement me")
+	q := `INSERT INTO teams (name) VALUES ($1)`
+
+	var insertedName string
+	err := s.getExecutor(ctx).QueryRow(ctx, q, name).Scan(&insertedName)
+	if err != nil {
+		var pgxError *pgconn.PgError
+		if errors.As(err, &pgxError) && pgxError.Code == UniqueViolationErr {
+			return "", errs.TeamExistsError{TeamName: name}
+		}
+		return "", fmt.Errorf("postgres failed to execute insert query for team: %w", err)
+	}
+
+	return insertedName, nil
 }
 
 func (s *Storage) GetTeam(ctx context.Context, name string) (*model.Team, error) {
