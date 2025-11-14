04556dbc6f5817f400b1e2e0aa62dca686414b50
trust-me-im-an-engineer <isavelij4@gmail.com>
2025-11-14 17:55:34 +0300

    separate storage interface

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -7,14 +7,25 @@ import (
 )
 
 type Storage interface {
+	Team
+	User
+	PullRequest
+}
+
+type Team interface {
 	AddTeam(ctx context.Context, name string) (string, error)
-	AddUpdateUsers(ctx context.Context, users []model.User) ([]model.User, error)
 	GetTeam(ctx context.Context, name string) (*model.Team, error)
+}
+
+type User interface {
+	AddUpdateUsers(ctx context.Context, users []model.User) ([]model.User, error)
 	SetUserActivity(ctx context.Context, id string, active bool) (*model.User, error)
 
 	// GetActiveColleges returns userIDs of users in the same team as userID excluding userID itself.
 	GetActiveColleges(ctx context.Context, userID string) ([]string, error)
+}
 
+type PullRequest interface {
 	CreatePullRequest(ctx context.Context, pr *model.PullRequest) (*model.PullRequest, error)
 	GetPullRequest(ctx context.Context, id string) (*model.PullRequest, error)
 	UpdatePullRequest(ctx context.Context, pr *model.PullRequest) (*model.PullRequest, error)
